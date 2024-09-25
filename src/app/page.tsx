c1b79e6a9cffe7e86d66f534846a98ef445e0641
MonikySouzaLopes <monikysouzalopes@gmail.com>
2024-09-24 20:49:53 -0400

    Inicialização do trab

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [user, setUser] = useState(true);
+ if(user){
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Cliente</h1>
+      <button onClick={() => setUser(false)}>Oi</button>
+    </div>
+  );
+ }else{
+  return(
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <h1>Administrador</h1>
+    <button onClick={() => setUser(true)}>Hi</button>
+  </div>
+  );
+ }
+}
